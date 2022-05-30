c36684f435613b8ce58060e751b902d4b80fc66f
Ciaran De Groot <ciaran.deg@gmail.com>
2022-05-30 02:10:59 -0600

    Get rid of description

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.stdenv.mkDerivation {
-      pname = "comp58";
-      version = "1.0";
-      src = ./.;
-      buildInputs = [pkgs.faust2jack];
-      buildPhase = "faust2jack ./comp58.dsp";
-      installPhase = ''
-        mkdir -p $out/bin
-        cp comp58 $out/bin
-      '';
-    };
-
+    in
+      pkgs.stdenv.mkDerivation {
+        pname = "comp58";
+        version = "1.0";
+        src = ./.;
+        buildInputs = [pkgs.faust2jack];
+        buildPhase = "faust2jack ./comp58.dsp";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp comp58 $out/bin
+        '';
+      };
   };
 }
